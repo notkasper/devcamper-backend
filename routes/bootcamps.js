973f7253461b5039f9643d945cccbb5cc3ca58bd
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-19 19:34:18 +0200

    get courses in bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,8 +7,13 @@ const {
   updateBootcamp,
   getBootcampsInRadius
 } = require('../controllers/bootcamps');
+// Include other resource routers
+const courseRouter = require('./courses');
 
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distanceKm').get(getBootcampsInRadius);
 
