aad3a6a56138702f2a6ed53d52cbcbd6d89a0e27
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-19 17:42:16 +0200

    build seeder

---
+++ seeder.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const Bootcamp = require('./models/Bootcamp');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data imported...'.green.inverse);
+    process.exit(0);
+  } catch (error) {
+    console.error(`Error while creating bootcamps\n${error}`);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data deleted...'.red.inverse);
+    process.exit(0);
+  } catch (error) {
+    console.error(`Error while deleting bootcamps\n${error}`);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+}
+
+if (process.argv[2] === '-d') {
+  deleteData();
+}
