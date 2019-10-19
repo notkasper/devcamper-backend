eaa528cff9f47ccf827b065167a8fae8a6e666ef
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-19 17:17:45 +0200

    geocoding

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -104,6 +105,25 @@ const BootcampSchema = new mongoose.Schema(
 // not using arrow function because they handle the 'this' keyword differently
 BootcampSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const location = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [location[0].longitude, location[0].latitude],
+    formattedAddress: location[0].formattedAddress,
+    street: location[0].streetName,
+    city: location[0].city,
+    state: location[0].state,
+    zipcode: location[0].zipcode,
+    country: location[0].countryCode
+  };
+
+  // do not save address
+  this.address = undefined;
   next();
 });
 
