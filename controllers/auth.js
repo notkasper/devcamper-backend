4adc6aabb314038ff64ccbb595edaa7c30a448d1
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-20 00:55:01 +0200

    login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,3 +21,37 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ success: true, token });
 });
+
+// @desc    Login User
+// @route   POST /api/v1/auth/register
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    next(new ErrorResponse('Please provide an email and password', 400));
+    return;
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    next(new ErrorResponse(`Invalid credentials`, 401));
+    return;
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    next(new ErrorResponse(`Invalid credentials`, 401));
+    return;
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
+});
--- models/User.js
+++ models/User.js
@@ -45,4 +45,9 @@ UserSchema.methods.getSignedJwtToken = function() {
   });
 };
 
+// match password
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model('User', UserSchema);
