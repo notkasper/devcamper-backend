072863c8c5b195b81468feff07d63ef8e2177d38
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-20 00:35:40 +0200

    create user and hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,15 @@ const asyncHandler = require('../middleware/async');
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  res.status(201).json({ success: true, data: user.id });
 });
