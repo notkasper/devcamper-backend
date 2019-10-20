9848d1842718dccfa71754cd273291c06c21f65d
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-20 12:35:15 +0200

    secure cookie in production

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,6 +60,10 @@ const sendTokenResponse = (user, statusCode, res) => {
     httpOnly: true
   };
 
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
   res
     .status(statusCode)
     .cookie('token', token, options)
