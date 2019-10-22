85e511aef4fac3116f3f7124284a7b7d9c6e8848
Kasper <kasperkarelse98@gmail.com>
2019-10-22 22:41:08 +0200

    get single review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -22,3 +22,19 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
     res.status(200).json(res.advancedResults);
   }
 });
+
+// @desc    Get single review
+// @route   GET /api/v1/review/:id
+// @access  Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({ path: 'bootcamp', select: 'name description' });
+
+  if (!review) {
+    return next(new ErrorResponse(`Review with id ${req.params.id} does not exist`, 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: review
+  });
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview } = require('../controllers/reviews');
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
@@ -13,5 +13,7 @@ router.route('/').get(
   }),
   getReviews
 );
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
